93d034b21035cf3ec5636e32a033218a4372c56d
Robert <rmoryson@man.poznan.pl>
2021-01-15 21:57:02 +0100

    Dodanie ViewModelu

---
+++ Open Auth/Open Auth/login/viewmodel/LoginViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewModel.swift
+//  test
+//
+//  Created by Robert Moryson on 15/01/2021.
+//
+
+import SwiftUI
+import Combine
+import Alamofire
+
+class LoginViewModel: ObservableObject {
+    @Published var phone: String = ""
+    @Published var showError = false
+    @Published var isSmsSent = false
+    @Published var error: LoginError?
+
+    private var sendSmsCancellable: AnyCancellable?
+    private var networkManager = NetworkManager()
+    
+    func sendSms(phone: String) {
+        sendSmsCancellable = networkManager.sendSms(phone: phone)
+            .sink(receiveCompletion: { [weak self] result in
+                switch result {
+                case .failure(let error):
+                    self?.error = error
+                    self?.showError = true
+                case .finished:
+                    print("LoginViewModel: Wysłano wiadomość SMS")
+                }
+            }, receiveValue: { [weak self] isSmsSent in
+                self?.isSmsSent = isSmsSent
+            })
+    }
+}
+
